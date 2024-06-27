414ef2b103412256a520bb8ea716a38bff889693
twinklegenon <twinklegenon7@gmail.com>
2024-06-27 12:20:19 +0800

    initial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div  className='MainContainer'>
+        <div className='DetailsBox'></div>
+        <div className='MainContent'>
+        <div className="navigation">
+              <h1>Home</h1>
+            </div>
+
+
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
